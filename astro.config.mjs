930018224f665306f0b0397df245627564e291ee
Sam Goudie <hello@sgoudie.com>
2025-09-13 17:31:07 +0100

    Added vercel adapter and redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({}),
   vite: {
     plugins: [tailwindcss()]
   }
